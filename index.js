7d0576f53d92ce3c8b66156f382485a5d31bdeae
Ilya Minyaev <ayli.veaynim@gmail.com>
2017-06-02 12:23:07 +0300

    Disable cache when serving static

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 
-var path = require("path");
+const path = require('path');
 const express = require('express');
 const axios = require('axios');
+const serveStatic = require('serve-static');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -10,7 +11,10 @@ const app = express();
 // Define routes
 app.use(require('./routes'));
 // Define path or admin page
-app.use('/', express.static('./public'));
+app.use(serveStatic(path.join(__dirname, 'public'), {
+  cacheControl: false
+}));
+//app.use('/', express.static('./public'));
 // Set rendering mechanism. Though not used anywhere for now
 app.set('views', './views');
 app.set('view engine', 'pug');
